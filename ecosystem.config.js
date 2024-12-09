66744efee580482d0ebf4ca025d9591ec2b0a1f9
lucianoojeda36 <lucianoojeda36@gmail.com>
2024-12-09 15:08:04 -0300

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,11 @@ module.exports = {
   apps: [
     {
       name: 'comercio-server', // Nombre de la aplicación que aparecerá en PM2
-      script: 'node', // Usa node como el script para ejecutar
-      args: './dist/server.js', // El archivo JS compilado que ejecutará Node
-      interpreter: 'xvfb-run', // Especifica xvfb-run como el intérprete para la ejecución
-      cwd: './', // Directorio de trabajo de tu proyecto (asegúrate de que este sea correcto)
+      script: './dist/server.js', // El archivo JS compilado que ejecutará Node
+      interpreter: 'xvfb-run', // Usa xvfb-run como el intérprete
+      interpreter_args: 'node', // Argumentos para el intérprete (xvfb-run ejecutará node)
+      args: '', // No necesitas argumentos adicionales para el script
+      cwd: './', // Directorio de trabajo de tu proyecto
       watch: false, // Puedes desactivar el watch en producción si no es necesario
       env: {
         NODE_ENV: 'production', // Establece el entorno de la aplicación como 'production'
