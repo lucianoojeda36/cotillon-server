1d69ac0c314abdcf38d489218dc3e224de4ea09d
lucianoojeda36 <lucianoojeda36@gmail.com>
2024-12-10 10:16:30 -0300

    update

--- src/controllers/scraperController.ts
+++ src/controllers/scraperController.ts
@@ -1,11 +1,48 @@
 import { Request, Response } from 'express';
 import { Cluster } from 'puppeteer-cluster';
 import scrapeData from '../services/scraperService';
+import os from 'os';
+import { existsSync } from 'fs';
 
 let cluster: Cluster<any, any> | null = null;
 
+function getExecutablePath(): string {
+  const platform = os.platform();
+
+  if (platform === 'darwin') {
+    // macOS
+    const macPath =
+      '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
+    if (existsSync(macPath)) {
+      return macPath;
+    }
+  } else if (platform === 'linux') {
+    // Linux
+    const linuxPaths = ['/usr/bin/google-chrome', '/usr/bin/chromium'];
+    for (const path of linuxPaths) {
+      if (existsSync(path)) {
+        return path;
+      }
+    }
+  } else if (platform === 'win32') {
+    // Windows
+    const winPaths = [
+      'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+      'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
+    ];
+    for (const path of winPaths) {
+      if (existsSync(path)) {
+        return path;
+      }
+    }
+  }
+
+  throw new Error('No suitable Chrome/Chromium executable found!');
+}
+
 async function initializeCluster() {
   if (!cluster) {
+    const executablePath = getExecutablePath();
     cluster = await Cluster.launch({
       concurrency: Cluster.CONCURRENCY_BROWSER,
       maxConcurrency: 5, // Número máximo de navegadores concurrentes
@@ -17,7 +54,7 @@ async function initializeCluster() {
           '--disable-dev-shm-usage',
           '--disable-gpu',
         ],
-        executablePath: '/usr/bin/google-chrome',
+        executablePath,
       },
     });
 
