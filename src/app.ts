4579ce8c8274f5f487a6d1416da2f3e2705ebd83
lucianoojeda36 <lucianoojeda36@gmail.com>
2024-12-26 16:12:24 -0300

    update

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 console.log('--eee-aaa-rrrrheegolea');
+console.log('--eee-aaa-rrrrheegolea');
 
 app.use('/api', scrapeRoutes);
 app.use('/api/users', usersRoutes);
